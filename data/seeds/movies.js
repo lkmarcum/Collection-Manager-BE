d34b5201bc46f254e61bf7bcf12619d191c46d8a
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-03-31 12:27:56 -0500

    including second seed file update

--- data/seeds/movies.js
+++ data/seeds/movies.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("movies").insert([
         { collection_id: 1, title: "Side Effects", genre: "Drama" },
         { collection_id: 1, title: "Wedding Crashers", genre: "Comedy" },
         { collection_id: 1, title: "Law Abiding Citizen", genre: "Action" },
