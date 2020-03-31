b32731067cf54a0bf857744cf4537114c7eb7f94
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-03-31 12:27:34 -0500

    more bug fixes in seed files

--- data/seeds/collections.js
+++ data/seeds/collections.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("table_name").insert([
+      return knex("collections").insert([
         { owner_id: 1, title: "User 1's Movies" },
         { owner_id: 3, title: "User 2's Movies" },
         { owner_id: 2, title: "User 3's Movies" }
