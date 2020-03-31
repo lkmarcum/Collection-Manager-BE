a981e728026b979e80cbcf1c6181fb15e13ee17f
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-03-31 12:31:15 -0500

    remove truncate() from users seed

--- data/seeds/collections.js
+++ data/seeds/collections.js
@@ -6,8 +6,8 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("collections").insert([
         { owner_id: 1, title: "User 1's Movies" },
-        { owner_id: 3, title: "User 2's Movies" },
-        { owner_id: 2, title: "User 3's Movies" }
+        { owner_id: 3, title: "User 3's Movies" },
+        { owner_id: 2, title: "User 2's Movies" }
       ]);
     });
 };
