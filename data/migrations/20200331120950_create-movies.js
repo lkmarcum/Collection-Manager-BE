36d428ca805536d81612c27cb4599d3692331a9d
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-03-31 12:15:28 -0500

    created migration for movies

--- data/migrations/20200331120950_create-movies.js
+++ data/migrations/20200331120950_create-movies.js
@@ -1,21 +1,22 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("collections", tbl => {
+  return knex.schema.createTable("movies", tbl => {
     tbl.increments();
 
     tbl
-      .integer("owner_id")
+      .integer("collection_id")
       .unsigned()
       .notNullable()
       .references("id")
-      .inTable("users")
+      .inTable("collections")
       .onDelete("CASCADE")
       .onUpdate("CASCADE")
       .index();
     tbl.string("title", 255).notNullable();
+    tbl.string("genre", 255).notNullable();
   });
 };
 
 exports.down = function(knex, Promise) {
   // undo the operation in up
-  return knex.schema.dropTableIfExists("collections");
+  return knex.schema.dropTableIfExists("movies");
 };
