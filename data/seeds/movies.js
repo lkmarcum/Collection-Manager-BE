2a80d2e6edd2f29442346e5cb2280ecb76debcb0
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-03-31 12:55:16 -0500

    seed fix

--- data/seeds/movies.js
+++ data/seeds/movies.js
@@ -5,19 +5,19 @@ exports.seed = function(knex) {
     .then(function() {
       // Inserts seed entries
       return knex("movies").insert([
-        { collection_id: 13, title: "Side Effects", genre: "Drama" },
-        { collection_id: 13, title: "Wedding Crashers", genre: "Comedy" },
-        { collection_id: 13, title: "Law Abiding Citizen", genre: "Action" },
+        { collection_id: 19, title: "Side Effects", genre: "Drama" },
+        { collection_id: 19, title: "Wedding Crashers", genre: "Comedy" },
+        { collection_id: 19, title: "Law Abiding Citizen", genre: "Action" },
         {
-          collection_id: 14,
+          collection_id: 20,
           title: "Crazy Stupid Love",
           genre: "Romantic Comedy"
         },
-        { collection_id: 14, title: "Lincoln", genre: "Drama" },
-        { collection_id: 14, title: "The Rookie", genre: "Sports" },
-        { collection_id: 15, title: "12 Angry Men", genre: "Drama" },
-        { collection_id: 15, title: "A Few Good Men", genre: "Drama" },
-        { collection_id: 15, title: "The Martian", genre: "Sci-Fi" }
+        { collection_id: 20, title: "Lincoln", genre: "Drama" },
+        { collection_id: 20, title: "The Rookie", genre: "Sports" },
+        { collection_id: 21, title: "12 Angry Men", genre: "Drama" },
+        { collection_id: 21, title: "A Few Good Men", genre: "Drama" },
+        { collection_id: 21, title: "The Martian", genre: "Sci-Fi" }
       ]);
     });
 };
