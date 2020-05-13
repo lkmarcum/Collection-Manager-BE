bcd8f253d441e4338b3049520d5ccb66937ba418
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2020-05-13 16:29:52 -0500

    changed findByOwner query to raw in attempt to fix bug

--- collections/collectionModel.js
+++ collections/collectionModel.js
@@ -1,4 +1,5 @@
 const db = require("../data/dbConfig.js");
+const knex = require("knex");
 
 module.exports = {
   insert,
@@ -11,14 +12,15 @@ async function insert(collection) {
   return db("collections").insert(collection).returning("id");
 }
 
+function getAll() {
+  return db("collections");
+}
+
 function findByOwner(owner_id) {
-  return db("collections").where({ owner_id });
+  // return db("collections").where({ owner_id });
+  return knex.raw(`SELECT * from collections where owner_id = ${owner_id}`);
 }
 
 function findById(id, table) {
   return db(table).where({ collection_id: id });
-}
-
-function getAll() {
-  return db("collections");
 }
